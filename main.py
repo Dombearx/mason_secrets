fdb595ff03d14b19a473907b4267a7ce7814509b
dombearx <domin.dober@gmail.com>
2023-04-19 21:12:27 +0200

    Refactor

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
-from typing import Tuple
+from typing import Tuple, List
 
 
-def calculate(expression: str) -> Tuple[float, float]:
+def calculate(expression: str) -> float:
     expression_array = expression.split(" ")
+    result, _ = calculate_part(expression_array)
+    return result
+
+
+def calculate_part(expression_array: List[str]) -> Tuple[float, float]:
     operand = expression_array[0]
     if expression_array[1].isdigit():
         first_val = float(expression_array[1])
         second_val_pointer = 2
     else:
-        first_val, inner_pointer = calculate(" ".join(expression_array[1:]))
+        first_val, inner_pointer = calculate_part(expression_array[1:])
         second_val_pointer = 2 + inner_pointer
 
     if expression_array[second_val_pointer].isdigit():
         second_val = float(expression_array[second_val_pointer])
     else:
-        second_val, _ = calculate(" ".join(expression_array[second_val_pointer:]))
+        second_val, _ = calculate_part(expression_array[second_val_pointer:])
 
     return make_operation(operand, first_val, second_val), second_val_pointer
 
@@ -24,12 +29,9 @@ def make_operation(operand: str, first_val: float, second_val: float) -> float:
 
 
 def main():
-    result, _ = calculate("* 3 4")
-    print(result)
-    result, _ = calculate("* + 3 4 5")
-    print(result)
-    result, _ = calculate("/ - 3 4 + 2 5")
-    print(result)
+    print(calculate("* 3 4"))
+    print(calculate("* + 3 4 5"))
+    print(calculate("/ - 3 4 + 2 5"))
 
 
 if __name__ == '__main__':
